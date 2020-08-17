01454a6f579bae786495d57867488800ba315aff
Garrett Brown <gman9mm@live.com>
2020-08-17 15:19:15 -0700

    Allow answers to run on loops.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,17 @@
 $(document).ready(function () {
   $("#formOne").submit(function (event) {
     event.preventDefault();
-    const person1Input = $("input#person1").val();
-    console.log(person1Input);
-    const person2Input = $("input#person2").val();
-    const animalInput = $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    let inputs = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+    inputs.forEach(function (input) {
+      let all = $("input#" + input).val();
+      $("." + input).text(all);
+    });
 
     $("#story").show();
   });
 });
+
+
+
+
